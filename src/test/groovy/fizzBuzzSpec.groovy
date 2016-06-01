8b57c859e62173c1ff4bdf4f8c150e7a2ec49099
msomji <maisam.somji@gmail.com>
2016-06-01 11:18:02 -0400

    refactored all tests into one using where:

--- src/test/groovy/fizzBuzzSpec.groovy
+++ src/test/groovy/fizzBuzzSpec.groovy
@@ -6,38 +6,20 @@ class fizzBuzzSpec extends Specification {
 
     def  fizzBuzz = new FizzBuzz()
 
-    def "should return 1 when one is evaluated" (){
+    def "should fizz and buzz and fizzbuzz accurately" (){
         expect:
-        fizzBuzz.eval(1) == "1"
+        fizzBuzz.eval(input) == output
+
+        where:
+        input | output
+        1   |   "1"
+        2   |   "2"
+        3   |   "fizz"
+        6   |   "fizz"
+        5   |   "buzz"
+        10  |   "buzz"
+        15  |   "fizzBuzz"
+
     }
 
-    def "should evaluate to 2 when input is 2" () {
-        expect:
-        fizzBuzz.eval(2) == "2"
-    }
-
-    def "should return fizz when input is 3"() {
-        expect:
-        fizzBuzz.eval(3) == "fizz"
-    }
-
-    def "should return fizz when input is 6"() {
-        expect:
-        fizzBuzz.eval(6) == "fizz"
-    }
-
-    def "should return buzz when input is 5"() {
-        expect:
-        fizzBuzz.eval(5) == "buzz"
-    }
-
-    def "should return buzz when input is 10"() {
-        expect:
-        fizzBuzz.eval(10) == "buzz"
-    }
-
-    def "should return fizzBuzz when input is 15"() {
-        expect:
-        fizzBuzz.eval(15) == "fizzBuzz"
-    }
 }
