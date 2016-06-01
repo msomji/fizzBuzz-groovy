cf77f2f049b22af5c6a31eb203b66e9be389ff40
msomji <maisam.somji@gmail.com>
2016-06-01 10:22:03 -0400

    passing basic fizzbuzz tests

--- src/test/groovy/fizzBuzzSpec.groovy
+++ src/test/groovy/fizzBuzzSpec.groovy
@@ -1,6 +1,18 @@
+
 import spock.lang.Specification
 
 
 class fizzBuzzSpec extends Specification {
 
+    def  fizzBuzz = new FizzBuzz()
+
+    def "should return 1 when one is evaluated" (){
+        expect:
+        fizzBuzz.eval(1) == 1
+    }
+
+    def "should evaluate to 2 when input is 2" () {
+        expect
+        fizzBuzz.eval(2) == 2
+    }
 }
