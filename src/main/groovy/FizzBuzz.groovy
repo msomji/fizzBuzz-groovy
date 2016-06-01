0b9579ce527e498dd53f5e6198aad3b922542001
msomji <maisam.somji@gmail.com>
2016-06-01 11:15:15 -0400

    fizzing and buzzing and fizzBuzzing

--- src/main/groovy/FizzBuzz.groovy
+++ src/main/groovy/FizzBuzz.groovy
@@ -1,5 +1,11 @@
 class FizzBuzz {
-    int eval(int number) {
-        number
+     String eval(int number) {
+        if(number % 15 == 0)
+            return "fizzBuzz"
+         if (number % 3 == 0)
+            return "fizz"
+         if (number % 5 == 0)
+             return "buzz"
+        return number.toString()
     }
 }
--- src/test/groovy/fizzBuzzSpec.groovy
+++ src/test/groovy/fizzBuzzSpec.groovy
@@ -8,11 +8,36 @@ class fizzBuzzSpec extends Specification {
 
     def "should return 1 when one is evaluated" (){
         expect:
-        fizzBuzz.eval(1) == 1
+        fizzBuzz.eval(1) == "1"
     }
 
     def "should evaluate to 2 when input is 2" () {
-        expect
-        fizzBuzz.eval(2) == 2
+        expect:
+        fizzBuzz.eval(2) == "2"
+    }
+
+    def "should return fizz when input is 3"() {
+        expect:
+        fizzBuzz.eval(3) == "fizz"
+    }
+
+    def "should return fizz when input is 6"() {
+        expect:
+        fizzBuzz.eval(6) == "fizz"
+    }
+
+    def "should return buzz when input is 5"() {
+        expect:
+        fizzBuzz.eval(5) == "buzz"
+    }
+
+    def "should return buzz when input is 10"() {
+        expect:
+        fizzBuzz.eval(10) == "buzz"
+    }
+
+    def "should return fizzBuzz when input is 15"() {
+        expect:
+        fizzBuzz.eval(15) == "fizzBuzz"
     }
 }
